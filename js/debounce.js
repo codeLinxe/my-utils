c4e8fb1ab1ee3b5c06b3683cc0450d4658ac4f99
XianHao <896264217@qq.com>
2021-10-08 16:36:40 +0800

    Update debounce.js

--- js/debounce.js
+++ js/debounce.js
@@ -1,3 +1,7 @@
+// https://juejin.cn/post/7016502001911463950
+// 对于防抖和节流一个最主观的判断方法就是：在10s内你疯狂点击一个按钮，如果使用了防抖则会只执行一次，而你使用了节流则会每隔一段时间执行一次，这个时间可以自己来掌控。
+
+// 防抖
 function debounce(fun, time) {
     let timer
     return function() {
@@ -9,6 +13,7 @@ function debounce(fun, time) {
     }
 }
 
+// 节流
 function throttle(fun, time) {
     let t1 = 0 //初始时间
     return function() {
