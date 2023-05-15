5579f2ae513f4adedaee4d40f0e803e0eeec3862
XianHao <896264217@qq.com>
2023-05-15 17:15:25 +0800

    Update getZeroTimestamp.js

--- js/getZeroTimestamp.js
+++ js/getZeroTimestamp.js
@@ -1,3 +1,7 @@
 function getZeroTimestamp() {
-  return new Date().setHours(0, 0, 0, 0).getTime()
+  return new Date().setHours(0, 0, 0, 0)
 }
+
+const result = getZeroTimestamp()
+
+console.log('result', result)
