67eb0a26b07b13304e8c86a23144aae281d7c711
XianHao <896264217@qq.com>
2024-05-17 10:58:30 +0800

    Create shuffle.js

---
+++ js/shuffle.js
@@ -0,0 +1,28 @@
+// 随机排列数组的元素
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// 所有可能排列的出现次数
+let count = {
+  '123': 0,
+  '132': 0,
+  '213': 0,
+  '231': 0,
+  '321': 0,
+  '312': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+  let array = [1, 2, 3];
+  shuffle(array);
+  count[array.join('')]++;
+}
+
+// 显示所有可能排列的出现次数
+for (let key in count) {
+  alert(`${key}: ${count[key]}`);
+}
